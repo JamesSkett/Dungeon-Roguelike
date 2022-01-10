84b29d7d7b5f4c38fd504c35dda64ef276655594
James <jamesskett31@gmail.com>
2022-01-10 21:59:31 +0000

    Playing with images

--- Dungeon Roguelike/Dungeon Roguelike/Renderer.cpp
+++ Dungeon Roguelike/Dungeon Roguelike/Renderer.cpp
@@ -2,19 +2,29 @@
 
 Renderer::Renderer()
 {
+    al_init();
+    al_install_keyboard();
+
+    timer = al_create_timer(1.0 / 30.0);
+    queue = al_create_event_queue();
+    disp = al_create_display(1920, 1080);
+    font = al_create_builtin_font();
+
+    if (!al_init_image_addon())
+    {
+        printf("couldn't initialize image addon\n");
+    }
+
+    image = al_load_bitmap("turtle.jpg");
+    if (!image)
+    {
+        printf("Couldnt Load image\n");
+    }
 
 }
 
 void Renderer::Init()
 {
-    al_init();
-    al_install_keyboard();
-
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
-    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    ALLEGRO_DISPLAY* disp = al_create_display(1280, 720);
-    ALLEGRO_FONT* font = al_create_builtin_font();
-
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
@@ -35,20 +45,25 @@ void Renderer::Init()
         if (redraw && al_is_event_queue_empty(queue))
         {
             al_clear_to_color(al_map_rgb(0, 0, 0));
+            al_draw_bitmap(image, 0, 0, 0);
+
             al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+
+
             al_flip_display();
 
             redraw = false;
         }
     }
 
-    al_destroy_font(font);
-    al_destroy_display(disp);
-    al_destroy_timer(timer);
-    al_destroy_event_queue(queue);
-
 }
 
 Renderer::~Renderer()
 {
+    al_destroy_bitmap(image);
+
+    al_destroy_font(font);
+    al_destroy_display(disp);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
 }
--- Dungeon Roguelike/Dungeon Roguelike/Renderer.h
+++ Dungeon Roguelike/Dungeon Roguelike/Renderer.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <stdio.h>
+
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
 
 class Renderer
 {
@@ -13,6 +16,11 @@ public:
 	~Renderer();
 
 private:
+	ALLEGRO_TIMER* timer;
+	ALLEGRO_EVENT_QUEUE* queue;
+	ALLEGRO_DISPLAY* disp;
+	ALLEGRO_FONT* font;
 
+	ALLEGRO_BITMAP* image;
 };
 
